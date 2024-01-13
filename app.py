2b0a66d8f2bd6672b4c9006d23caae1690ed6450
whwhdtk <8whwhdtk8@naver.com>
2024-01-13 21:14:57 +0900

    자막 번역 및 리다이렉트 추가

--- app.py
+++ app.py
@@ -1,16 +1,23 @@
 import os
 import datetime
 import json
-from flask import Flask, render_template_string
+from flask import Flask, render_template_string, redirect
 from pytube import YouTube
-from youtube_transcript_api import YouTubeTranscriptApi
+from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound
+import deepl
 import pysftp
 
+def jsonTrans(srt_json):
+    global inpJson
+    translator = deepl.Translator(inpJson["deepl_auth_key"])
+    for i in srt_json:
+        i["text"] = translator.translate_text(i["text"], target_lang="KO-KR")
+    return
 
 def json2srt(data):
     srt_content = ""
     for index, entry in enumerate(data):
-        # 자막 번호 추가
+        # 자막 번호 추가[]
         srt_content += f"{index + 1}\n"
 
         # 시작 시간과 종료 시간 계산
@@ -18,14 +25,14 @@ def json2srt(data):
         end = start + datetime.timedelta(seconds=entry['duration'])
 
         # 시간 형식 변환 (시:분:초,밀리초)
-        start_str = str(start)[:-3].replace('.', ',')
-        end_str = str(end)[:-3].replace('.', ',')
-
-        # 00:00 형식을 00:00:00 형식으로 변환
-        if len(start_str.split(':')) == 2:
-            start_str = f"00:{start_str}"
-        if len(end_str.split(':')) == 2:
-            end_str = f"00:{end_str}"
+        start_str = str(int(start.total_seconds() // 3600)).zfill(2) + ':' + \
+                    str(int(start.total_seconds() % 3600 // 60)).zfill(2) + ':' + \
+                    str(int(start.total_seconds() % 60)).zfill(2) + ',' + \
+                    str(int(start.microseconds / 1000)).zfill(3)
+        end_str = str(int(end.total_seconds() // 3600)).zfill(2) + ':' + \
+                  str(int(end.total_seconds() % 3600 // 60)).zfill(2) + ':' + \
+                  str(int(end.total_seconds() % 60)).zfill(2) + ',' + \
+                  str(int(end.microseconds / 1000)).zfill(3)
 
         # 자막 텍스트와 함께 SRT 형식으로 추가
         srt_content += f"{start_str} --> {end_str}\n{entry['text']}\n\n"
@@ -34,7 +41,9 @@ def json2srt(data):
 def mergeSource(vName, aName, srtName, outName):
     global inpJson
     #cmd = f'ffmpeg -i {vName} -i {aName} -i {srtName} -c:v copy -c:a aac -strict experimental -c:s mov_text -map 0:v -map 1:a -map 2:s {outName}' #내장식
-    cmd = f'ffmpeg -loglevel fatal -y -i {vName} -i {aName} -vf "subtitles={srtName}:fontsdir=/root/p:force_style=\'Fontname={inpJson["fontname"]}\'" -c:a aac -strict experimental {outName}'
+    #cmd = f'ffmpeg -loglevel fatal -y -i {vName} -i {aName} -vf "subtitles={srtName}:fontsdir=/root/p:force_style=\'Fontname={inpJson["fontname"]}\'" -c:a aac -strict experimental {outName}'
+    cmd = f"ffmpeg -loglevel fatal -y -i {vName} -i {aName} -vf \"subtitles={srtName}:fontsdir=/root/p:force_style='Fontname={inpJson['fontname']},Alignment=2,MarginV=30'\" -c:a aac -strict experimental {outName}"
+
     os.system(cmd)
     return
 
@@ -52,16 +61,24 @@ def routine(video_id: str):
     os.rename(dName, vName)
     dName = yt.streams.filter(only_audio=True).first().download()
     os.rename(dName, aName)
-    srt = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko'],)
-    srt = json2srt(srt)
+    try:
+        srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko'])
+    except NoTranscriptFound as e:
+        try:
+            srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+        except NoTranscriptFound as e:
+            srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ja'])
+        jsonTrans(srt_json)
+
+    srt = json2srt(srt_json)
     srtName = f'{video_id}.srt'
     with open(srtName, 'w', encoding='utf-8') as f:
         f.write(str(srt))
-
+    #배열에 text
     mergeSource(vName, aName, srtName, outName)
     os.remove(vName)
     os.remove(aName)
-    os.remove(srtName)
+    os.remove(srtName)  
 
     cnopts = pysftp.CnOpts()
     cnopts.hostkeys = None
@@ -92,7 +109,7 @@ def subscribe(id):
 
     routine(id)
     result = f"{inpJson['output_route']}{id}.mp4"
-    return render_template_string("<html><body><h1>{{ result }}</h1></body></html>", result=result)
+    return redirect(result)
 
 if __name__ == '__main__':
     global inpJson
