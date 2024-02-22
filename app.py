81e7d147768845e13f1ebcbcc365d9cbcfa92e08
whwhdtk <8whwhdtk8@naver.com>
2024-02-23 04:34:52 +0900

    비동기 프론트엔드 추가

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 import datetime
 import json
-from flask import Flask, render_template_string, redirect
+from flask import Flask, render_template_string, redirect, render_template, request, jsonify
 from pytube import YouTube
 from youtube_transcript_api import YouTubeTranscriptApi, NoTranscriptFound
 import deepl
@@ -11,7 +11,7 @@ def jsonTrans(srt_json):
     global inpJson
     translator = deepl.Translator(inpJson["deepl_auth_key"])
     for i in srt_json:
-        i["text"] = translator.translate_text(i["text"], target_lang="KO-KR")
+        i["text"] = translator.translate_text(i["text"], target_lang="KO")
     return
 
 def json2srt(data):
@@ -47,7 +47,7 @@ def mergeSource(vName, aName, srtName, outName):
     os.system(cmd)
     return
 
-def routine(video_id: str):
+def routine(video_id: str, la: str):
     global inpJson
 
     vName = f'{video_id}_Video'
@@ -62,14 +62,24 @@ def routine(video_id: str):
     dName = yt.streams.filter(only_audio=True).first().download()
     os.rename(dName, aName)
     try:
-        srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko'])
-    except NoTranscriptFound as e:
+        srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=[la])
+        if la != 'ko':
+            jsonTrans(srt_json)
+    except:
         try:
-            srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+            srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ko'])
         except NoTranscriptFound as e:
-            srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ja'])
-        jsonTrans(srt_json)
+            try:
+                srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['en'])
+                jsonTrans(srt_json)
+            except NoTranscriptFound as e:
+                try: 
+                    srt_json = YouTubeTranscriptApi.get_transcript(video_id, languages=['ja'])
+                    jsonTrans(srt_json)
+                except:
+                    return False
 
+    
     srt = json2srt(srt_json)
     srtName = f'{video_id}.srt'
     with open(srtName, 'w', encoding='utf-8') as f:
@@ -91,25 +101,54 @@ def routine(video_id: str):
     with pysftp.Connection(host, port=port, username=id, password=pw, cnopts=cnopts) as sftp:
         sftp.put(f"./{outName}", f"{sftpOutLocale}{outName}")
     os.remove(outName)
-    return
+    return True
+
+def getRemoteList():
+    global inpJson
+    ret = list()
+    cnopts = pysftp.CnOpts()
+    cnopts.hostkeys = None
+    host = inpJson["sftp"]["host"]
+    port = inpJson["sftp"]["port"]
+    id = inpJson["sftp"]["id"]
+    pw = inpJson["sftp"]["pw"]
+    sftpOutLocale = inpJson["sftp"]["locale"]
+    try:
+        with pysftp.Connection(host, port=port, username=id, password=pw, cnopts=cnopts) as sftp:
+            sftp.cwd(sftpOutLocale)
+            directory_attributes = sftp.listdir_attr()
+            for attr in directory_attributes:
+                ret.append(f"이름: {attr.filename}, 크기: {attr.st_size}, 수정 날짜: {attr.st_mtime}")
+    except Exception as e:
+        print(f"연결 중 에러 발생: {e}")
+
+    return ret
 
 app = Flask(__name__)
-
-@app.route('/')
-def home():
-    return ""
 
 @app.route('/subsc/')
 def info():
     return render_template_string("<html><body><h1>/subsc/[id]</h1></body></html>")
 
-@app.route('/subsc/<id>')
-def subscribe(id):
+@app.route('/subsc/<id>/<la>')
+def subscribe(id, la):
     global inpJson
 
-    routine(id)
-    result = f"{inpJson['output_route']}{id}.mp4"
-    return redirect(result)
+    success = routine(id, la)
+    # 성공 여부에 따른 응답 반환
+    if success:
+        return jsonify({"success": True, "message": "Subscription successful"}), 200
+    else:
+        return jsonify({"success": False, "message": "Subscription failed"}), 500
+
+@app.route('/getremotelist', methods=['GET'])
+def list_files():
+    file_list = getRemoteList()
+    return jsonify(file_list)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     global inpJson
